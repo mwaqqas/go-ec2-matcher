f89f0bd151118ccc4cfb3b37edf17d59f98fffba
mwaqqas <mohamed.waqqas@gmail.com>
2020-01-04 17:34:18 +0300

    Refactored code to use a list of structs. fixed issue where workoads with more than one price dimenstion were showing up multiple times in resultant csv. refactored main method to smaller functions.

--- calculate.go
+++ calculate.go
@@ -6,7 +6,7 @@ import (
 	"os"
 )
 
-type EC2Req struct {
+type EC2Item struct {
 	WorkloadName        string
 	InstanceType        string
 	Environment         string
@@ -22,155 +22,177 @@ type EC2Req struct {
 }
 
 func GetEC2Prices(csvIn string, csvOut string, region string) (err error) {
-	f, err := os.Open(csvIn)
+	records, err := readCSVFile(csvIn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var result []EC2Item
+	header := makeHeaderRow()
+	result = append(result, header)
+
+	index, err := extractEC2Product("data/ec2_offer_" + region + ".json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, record := range records {
+		item := makeItemFromCSVRow(record)
+		prodSKU := findEC2ItemSKU(item, index.Products)
+		switch item.PaymentModel {
+		case "OnDemand":
+			r, found := findOnDemandOffer(prodSKU, item, index.Terms.OnDemand)
+			if found {
+				result = append(result, r)
+			}
+		case "Reserved":
+			r, found := findReservedOffer(prodSKU, item, index.Terms.Reserved)
+			if found {
+				result = append(result, r)
+			}
+		}
+	}
+
+	err = writeCSV(csvOut, result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return
+
+}
+
+func readCSVFile(filename string) (rows [][]string, err error) {
+	f, err := os.Open(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
 
 	r := csv.NewReader(f)
+
 	// skip the header row
 	if _, err := r.Read(); err != nil {
 		log.Fatal(err)
 	}
 
 	// process the rest of the records
-	records, err := r.ReadAll()
-	if err != nil {
-		log.Fatal(err)
-	}
-	var out [][]string
-	out = append(out, []string{
-		"WorkloadName",
-		"InstanceType",
-		"Environment",
-		"OS",
-		"PreInstalledSw",
-		"Tenancy",
-		"PaymentModel",
-		"LeaseContractLength",
-		"OfferingClass",
-		"PurchaseOption",
-		"ProductSKU",
-		"Upfront Fee",
-		"Hourly Price",
-	})
+	rows, err = r.ReadAll()
+	return
+}
 
-	index, err := extractEC2Product("data/ec2_offer_" + region + ".json")
-	if err != nil {
-		log.Fatal(err)
+func makeHeaderRow() EC2Item {
+	return EC2Item{
+		WorkloadName:        "WorkloadName",
+		InstanceType:        "InstanceType",
+		Environment:         "Environment",
+		OS:                  "OS",
+		PreInstalledSw:      "PreInstalledSw",
+		Tenancy:             "Tenancy",
+		PaymentModel:        "PaymentModel",
+		LeaseContractLength: "LeaseContractLength",
+		OfferingClass:       "OfferingClass",
+		PurchaseOption:      "PurchaseOption",
+		UpfrontFee:          "UpfrontFee (USD)",
+		HourlyFee:           "HourlyFee (USD)",
 	}
-	for _, record := range records {
-		req := EC2Req{
-			WorkloadName:        record[0],
-			InstanceType:        record[1],
-			Environment:         record[2],
-			OS:                  record[3],
-			PreInstalledSw:      record[4],
-			Tenancy:             record[5],
-			PaymentModel:        record[6],
-			LeaseContractLength: record[7],
-			OfferingClass:       record[8],
-			PurchaseOption:      record[9],
+}
+
+func makeItemFromCSVRow(record []string) EC2Item {
+	return EC2Item{
+		WorkloadName:        record[0],
+		InstanceType:        record[1],
+		Environment:         record[2],
+		OS:                  record[3],
+		PreInstalledSw:      record[4],
+		Tenancy:             record[5],
+		PaymentModel:        record[6],
+		LeaseContractLength: record[7],
+		OfferingClass:       record[8],
+		PurchaseOption:      record[9],
+	}
+}
+
+func findEC2ItemSKU(item EC2Item, productIndex map[string]ec2) (sku string) {
+	for _, product := range productIndex {
+		if product.Attributes.InstanceType == item.InstanceType &&
+			product.Attributes.PreInstalledSw == item.PreInstalledSw &&
+			product.Attributes.OperatingSystem == item.OS &&
+			product.Attributes.Tenancy == item.Tenancy &&
+			product.Attributes.Capacitystatus == "Used" {
+			return product.Sku
 		}
-		for prodK, prodV := range index.Products {
-			if prodV.Attributes.InstanceType == req.InstanceType &&
-				prodV.Attributes.PreInstalledSw == req.PreInstalledSw &&
-				prodV.Attributes.OperatingSystem == req.OS &&
-				prodV.Attributes.Tenancy == req.Tenancy &&
-				prodV.Attributes.Capacitystatus == "Used" {
-				switch req.PaymentModel {
-				case "OnDemand":
-					for prodSKU, termMap := range index.Terms.OnDemand {
-						if prodSKU == prodK {
-							for _, offerTerm := range termMap {
-								for _, pd := range offerTerm.PriceDimensions {
-									out = append(out, []string{
-										req.WorkloadName,
-										req.InstanceType,
-										req.Environment,
-										req.OS,
-										req.PreInstalledSw,
-										req.Tenancy,
-										req.PaymentModel,
-										req.LeaseContractLength,
-										req.OfferingClass,
-										req.PurchaseOption,
-										prodK,
-										"", //upfront fee
-										pd.PricePerUnit.USD,
-									})
-								}
-							}
-						}
-					}
-				case "Reserved":
-					for prodSKU, termMap := range index.Terms.Reserved {
-						if prodSKU == prodK {
-							for _, offerTerm := range termMap {
-								if offerTerm.TermAttributes.LeaseContractLength == req.LeaseContractLength &&
-									offerTerm.TermAttributes.OfferingClass == req.OfferingClass &&
-									offerTerm.TermAttributes.PurchaseOption == req.PurchaseOption {
-									for _, pd := range offerTerm.PriceDimensions {
-										if pd.Description == "Upfront Fee" {
-											out = append(out, []string{
-												req.WorkloadName,
-												req.InstanceType,
-												req.Environment,
-												req.OS,
-												req.PreInstalledSw,
-												req.Tenancy,
-												req.PaymentModel,
-												req.LeaseContractLength,
-												req.OfferingClass,
-												req.PurchaseOption,
-												prodK,
-												pd.PricePerUnit.USD, //upfront fee
-												"",
-											})
-										} else {
-											out = append(out, []string{
-												req.WorkloadName,
-												req.InstanceType,
-												req.Environment,
-												req.OS,
-												req.PreInstalledSw,
-												req.Tenancy,
-												req.PaymentModel,
-												req.LeaseContractLength,
-												req.OfferingClass,
-												req.PurchaseOption,
-												prodK,
-												"", //upfront fee
-												pd.PricePerUnit.USD,
-											})
-										}
-									}
-								}
-							}
-						}
-					}
+	}
+	return //empty
+}
+
+func findOnDemandOffer(sku string, item EC2Item, terms map[string]map[string]offerTerm) (out EC2Item, found bool) {
+	for k, v := range terms {
+		if k == sku {
+			for _, term := range v {
+				for _, pd := range term.PriceDimensions {
+					item.HourlyFee = pd.PricePerUnit.USD
+					return item, true
 				}
 			}
 		}
 	}
+	return item, false
+}
 
-	f, err = os.Create(csvOut)
+func findReservedOffer(sku string, item EC2Item, terms map[string]map[string]offerTerm) (out EC2Item, found bool) {
+	for k, v := range terms {
+		if k == sku {
+			for _, term := range v {
+				if term.TermAttributes.LeaseContractLength == item.LeaseContractLength &&
+					term.TermAttributes.OfferingClass == item.OfferingClass &&
+					term.TermAttributes.PurchaseOption == item.PurchaseOption {
+					for _, pd := range term.PriceDimensions {
+						if pd.Description == "Upfront Fee" {
+							item.UpfrontFee = pd.PricePerUnit.USD
+						} else {
+							item.HourlyFee = pd.PricePerUnit.USD
+						}
+					}
+					return item, true
+				}
+			}
+		}
+	}
+	return item, false
+}
+
+func writeCSV(filename string, contents []EC2Item) (err error) {
+	f, err := os.Create(filename)
 	if err != nil {
 		log.Fatal(err)
+		return err
 	}
 	defer f.Close()
 
 	writer := csv.NewWriter(f)
 	defer writer.Flush()
 
-	for _, value := range out {
-		err := writer.Write(value)
+	for _, item := range contents {
+		s := []string{
+			item.WorkloadName,
+			item.InstanceType,
+			item.Environment,
+			item.OS,
+			item.PreInstalledSw,
+			item.Tenancy,
+			item.PaymentModel,
+			item.LeaseContractLength,
+			item.OfferingClass,
+			item.PurchaseOption,
+			item.UpfrontFee,
+			item.HourlyFee,
+		}
+
+		err = writer.Write(s)
 		if err != nil {
 			log.Fatal(err)
+			return
 		}
 	}
-
 	return
-
 }
