e70d212cf3b018e25179e7e128d4f01024ac6dad
mwaqqas <mohamed.waqqas@gmail.com>
2020-01-10 13:51:25 +0300

    refactored to use os args instead of hardcoded values

--- cmd/go-ec2-matcher/main.go
+++ cmd/go-ec2-matcher/main.go
@@ -1,19 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/mwaqqas/go-ec2-matcher/pkg/ec2"
 )
 
 func main() {
-	err := ec2.GetEC2Prices(
-		"sample_data/ec2-requirements.csv",
-		"sample_data/ec2-prices-result.csv",
-		"me-south-1",
-	)
-	if err != nil {
-		log.Fatal(err)
+	args := os.Args[1:]
+	operation := args[0]
+	switch operation {
+	case "findPrice":
+		src, dest, region := args[1], args[2], args[3]
+		err := ec2.GetEC2Prices(
+			src,
+			dest,
+			region,
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+	case "findMatch":
+		src, region := args[1], args[2]
+		result, err := ec2.GetEC2Match(src, region)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(result)
+	default:
+		log.Fatal("Case not found")
 	}
 
 }
