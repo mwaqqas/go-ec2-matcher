2abd74a1698a754295134606444404683a40dcd9
mwaqqas <mohamed.waqqas@gmail.com>
2020-01-15 17:28:39 +0300

    changed to accepting json string as input which is mapped to a struct and passed to the simple search function

--- cmd/go-ec2-matcher/main.go
+++ cmd/go-ec2-matcher/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 
 	"github.com/mwaqqas/go-ec2-matcher/pkg/ec2"
 )
@@ -14,84 +13,25 @@ func main() {
 	args := os.Args[1:]
 	operation := args[0]
 	switch operation {
-	// case "findPrice":
-	// 	src, dest, region := args[1], args[2], args[3]
-	// 	err := ec2.GetEC2Prices(
-	// 		src,
-	// 		dest,
-	// 		region,
-	// 	)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// case "findMatch":
-	// 	if len(args) != 5 {
-	// 		fmt.Println("")
-	// 		fmt.Printf("Error: Required 4 arguments. Provided: %d\n", len(args))
-	// 		fmt.Println("--------")
-	// 		fmt.Printf("Usage: go run main.go findMatch [SRC_CSV_PATH] [REGION] [CPUFuzzFactor] [RAMFuzzFactor]\n")
-	// 		log.Fatal()
-	// 	}
-	// 	src, region := args[1], args[2]
-	// 	CPUFuzzFactor, err := strconv.Atoi(args[3])
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	RAMFuzzFactor, err := strconv.ParseFloat(args[4], 64)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	result, err := ec2.GetEC2Match(src, region, CPUFuzzFactor, RAMFuzzFactor)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	fmt.Println(result)
-	case "byCPU":
-		if len(args) != 8 {
+	case "SimpleSearch":
+		if len(args) != 2 {
 			fmt.Println("")
-			fmt.Printf("Error: Required 8 arguments. Provided: %d\n", len(args))
-			fmt.Println("--------")
-			fmt.Printf("Usage: go run main.go byCPU [REGION] [CPU int] [RAM float64] [CPUFuzzFactor int] [RAMFuzzFactor float64] [roundUp bool]\n")
+			fmt.Printf("Error: Required 2 arguments. Provided: %d\n", len(args))
 			log.Fatal()
 		}
 
-		region := args[1]
-
-		reqCPU, err := strconv.Atoi(args[2])
+		b := []byte(args[1])
+		var req ec2.SimpleSearchReq
+		err := json.Unmarshal(b, &req)
 		if err != nil {
 			log.Fatal(err)
 		}
-
-		reqRAM, err := strconv.ParseFloat(args[3], 64)
+		result := ec2.SimpleSearch(req)
+		out, err := json.MarshalIndent(result, "", "  ")
 		if err != nil {
 			log.Fatal(err)
 		}
-
-		cff, err := strconv.Atoi(args[4])
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		rff, err := strconv.ParseFloat(args[4], 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		roundUp, err := strconv.ParseBool(args[6])
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		unique, err := strconv.ParseBool(args[7])
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		result := ec2.ByCPUAndRAM(region, reqCPU, reqRAM, cff, rff, roundUp, unique)
-
-		j, err := json.Marshal(result)
-		fmt.Println(string(j))
-		// fmt.Println(result)
+		fmt.Println(string(out))
 
 	default:
 		log.Fatal("Case not found")
