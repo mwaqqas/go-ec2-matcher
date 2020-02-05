54b84520855c681eba5989832154076ea26a57b8
mwaqqas <mohamed.waqqas@gmail.com>
2020-02-05 15:41:04 +0300

    changed data path and uncommented constants

--- pkg/ec2/constants.go
+++ pkg/ec2/constants.go
@@ -1,11 +1,9 @@
 package ec2
 
-// const (
-// 	offerIndexURL   = "https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/index.json"
-// 	dataDirPath     = "./data"
-// 	offerIndexPath  = dataDirPath + "/offer_index.json"
-// 	regionIndexPath = dataDirPath + "/region_index.json"
-// 	baseURL         = "https://pricing.us-east-1.amazonaws.com"
-
-// 	onDemandOfferCode = ".JRTCKXETXF"
-// )
+const (
+	dataPath        = "/Users/waqqas/projects/go-ec2-matcher/data/ec2"
+	offerIndexURL   = "https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/index.json"
+	offerIndexPath  = dataPath + "/offer_index.json"
+	regionIndexPath = dataPath + "/region_index.json"
+	baseURL         = "https://pricing.us-east-1.amazonaws.com"
+)
